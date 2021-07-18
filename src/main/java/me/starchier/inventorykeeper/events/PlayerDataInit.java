fb701abaa8d5846d2dcfbf05b43fdef5e22beca1
Starchier <tiyanw8@hotmail.com>
2021-07-19 00:09:30 +0800

    Clean up useless code

--- src/main/java/me/starchier/inventorykeeper/events/PlayerDataInit.java
+++ src/main/java/me/starchier/inventorykeeper/events/PlayerDataInit.java
@@ -6,7 +6,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerDataInit implements Listener {
-    private DataManager dataManager;
+    private final DataManager dataManager;
     public PlayerDataInit(DataManager dataManager) {
         this.dataManager = dataManager;
     }
