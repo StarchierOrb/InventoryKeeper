a98e810c89e45039227c73ccc7161ec68c8b672c
Starchier <tiyanw8@hotmail.com>
2021-07-31 01:01:07 +0800

    fix

--- src/main/java/me/starchier/inventorykeeper/configurations/ItemsConfig.java
+++ src/main/java/me/starchier/inventorykeeper/configurations/ItemsConfig.java
@@ -15,42 +15,45 @@ public class ItemsConfig {
         File file = new File(plugin.getDataFolder(), "items.yml");
         YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
         config.options().header(MessagesUtil.getMessage("commit.items-header"));
-        //Config for default item
-        config.addDefault("items.default.item-id", "STICK");
         //I don't know if it will works.
-        Map<String, Object> defaultItem = config.getConfigurationSection("items.default").getValues(false);
-        if (defaultItem.size() == 1) {
-            config.addDefault("items.default.name", MessagesUtil.getConfigValue("default-item-name"));
-            config.addDefault("items.default.item-name", MessagesUtil.getConfigValue("default-item-name"));
-            config.addDefault("items.default.item-lore", MessagesUtil.getConfigArrayValue("default-item-lore"));
-            config.addDefault("items.default.item-enchantments", Collections.singletonList(
-                    (PluginHandler.IS_LEGACY ? Enchantment.DURABILITY.getName() + "-10" : "unbreaking-10")
-            ));
-            config.addDefault("items.default.save-exp", false);
-            config.addDefault("items.default.exp-lose-percentage", "10-30");
-            config.addDefault("items.default.hunger-level", "reset");
-            config.addDefault("items.default.saturation-level", "set,3,7");
-            config.addDefault("items.default.disabled-worlds", new ArrayList<>());
-            config.addDefault("items.default.run-commands-on-death", new ArrayList<>());
-            config.addDefault("items.default.run-random-commands-on-death", new ArrayList<>());
-            config.addDefault("items.default.run-commands-on-respawn", Collections.singletonList(
-                    "15|90|effect %player% minecraft:fire_resistance %random% 1"
-            ));
-            config.addDefault("items.default.run-random-commands-on-respawn", Arrays.asList(
-                    "10|30|effect %player% minecraft:strength %random% 1",
-                    "10|30|effect %player% minecraft:speed %random% 1"
-            ));
-            config.addDefault("items.default.filter-entities-list.is-blacklist", true);
-            config.addDefault("items.default.filter-entities-list.entities", new ArrayList<>());
-            config.addDefault("items.default.filter-entities-name.is-blacklist", true);
-            config.addDefault("items.default.filter-entities-name.names-list", new ArrayList<>());
-            config.addDefault("items.default.items-with-lore-to-be-removed-on-death", Collections.singletonList(
-                    "&6Soul bind"
-            ));
-            config.addDefault("items.default.death-message", MessagesUtil.getConfigValue("saved-inventory"));
-            config.addDefault("items.default.priority", 10);
+        try {
+            Map<String, Object> defaultItem = config.getConfigurationSection("items.default").getValues(false);
+            if (defaultItem.size() == 1) {
+                //Config for default item
+                config.addDefault("items.default.item-id", "STICK");
+                config.addDefault("items.default.name", MessagesUtil.getConfigValue("default-item-name"));
+                config.addDefault("items.default.item-name", MessagesUtil.getConfigValue("default-item-name"));
+                config.addDefault("items.default.item-lore", MessagesUtil.getConfigArrayValue("default-item-lore"));
+                config.addDefault("items.default.item-enchantments", Collections.singletonList(
+                        (PluginHandler.IS_LEGACY ? Enchantment.DURABILITY.getName() + "-10" : "unbreaking-10")
+                ));
+                config.addDefault("items.default.save-exp", false);
+                config.addDefault("items.default.exp-lose-percentage", "10-30");
+                config.addDefault("items.default.hunger-level", "reset");
+                config.addDefault("items.default.saturation-level", "set,3,7");
+                config.addDefault("items.default.disabled-worlds", new ArrayList<>());
+                config.addDefault("items.default.run-commands-on-death", new ArrayList<>());
+                config.addDefault("items.default.run-random-commands-on-death", new ArrayList<>());
+                config.addDefault("items.default.run-commands-on-respawn", Collections.singletonList(
+                        "15|90|effect %player% minecraft:fire_resistance %random% 1"
+                ));
+                config.addDefault("items.default.run-random-commands-on-respawn", Arrays.asList(
+                        "10|30|effect %player% minecraft:strength %random% 1",
+                        "10|30|effect %player% minecraft:speed %random% 1"
+                ));
+                config.addDefault("items.default.filter-entities-list.is-blacklist", true);
+                config.addDefault("items.default.filter-entities-list.entities", new ArrayList<>());
+                config.addDefault("items.default.filter-entities-name.is-blacklist", true);
+                config.addDefault("items.default.filter-entities-name.names-list", new ArrayList<>());
+                config.addDefault("items.default.items-with-lore-to-be-removed-on-death", Collections.singletonList(
+                        "&6Soul bind"
+                ));
+                config.addDefault("items.default.death-message", MessagesUtil.getConfigValue("saved-inventory"));
+                config.addDefault("items.default.priority", 10);
+            }
+            config.options().copyDefaults(true);
+        } catch (NullPointerException ignored) {
         }
-        config.options().copyDefaults(true);
         try {
             config.save(file);
         } catch (IOException e) {
